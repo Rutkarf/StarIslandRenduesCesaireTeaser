d4d4bda00e1ee146c7a9c8b89e0c42d953e90f87
Rutkarf <125037847+Rutkarf@users.noreply.github.com>
2023-06-16 11:31:01 +0200

    Add files via upload

---
+++ StarIsland/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Star'Island</title>
+</head>
+
+<body>
+    <header>
+           <nav>
+           <div class="navbar">
+  <img src="assets/upload/starisland.png" alt="Logo">
+  <div class="middle-links">
+    <a href="#">Gallerie</a>
+    <a href="#">Devenir VIP</a>
+    <a href="#">Serveur</a>
+  </div>
+  <div class="right-links">
+    <a href="#">Tutoriels</a>
+    <a href="#">Events</a>
+  </div>
+</div>
+</nav>
+
+</body>
+</html>
